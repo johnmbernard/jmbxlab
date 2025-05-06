f77966b47853379d04274161a4271b043cb06d81
johnmbernard <john.m.bernard@outlook.com>
2025-05-06 16:42:10 -0400

    added contact form modal

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleBubble(element) {
     element.classList.toggle('expanded');
 }
+
+function toggleContactForm() {
+    const modal = document.getElementById('contact-modal');
+    if (modal.style.display === 'flex') {
+        modal.style.display = 'none';
+    } else {
+        modal.style.display = 'flex';
+    }
+}
